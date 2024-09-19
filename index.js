ca2084336893da5499ded3af408322a90e70596e
uralbek-v1-0-0 <versiya.v1.0.0@gmail.com>
2024-09-19 20:58:49 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+document.getElementById("mavzu").innerHTML = "HTML";
 document.getElementById("logotip").innerHTML = "Uralbek UZ";
 document.getElementById("data").innerHTML = "04.02.2019";
-document.getElementById("mavzu").innerHTML = "HTML";
 document.getElementById("").innerHTML = "";
 document.getElementById("").innerHTML = "";
 document.getElementById("").innerHTML = "";
